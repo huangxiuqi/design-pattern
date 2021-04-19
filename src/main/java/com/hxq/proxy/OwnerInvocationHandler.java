486cebb7dc7eb20175085911eed0e97c3afbc0c9
huangxiuqi <huangxiuqi@outlook.com>
2021-04-19 21:25:29 +0800

    代理模式

---
+++ src/main/java/com/hxq/proxy/OwnerInvocationHandler.java
@@ -0,0 +1,31 @@
+package com.hxq.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class OwnerInvocationHandler implements InvocationHandler {
+
+    private final PersonBean person;
+
+    public OwnerInvocationHandler(PersonBean person) {
+        this.person = person;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        try {
+            if (method.getName().startsWith("get")) {
+                return method.invoke(person, args);
+            } else if (method.getName().equals("setHotOrNotRating")) {
+                throw new IllegalAccessException();
+            } else if (method.getName().startsWith("set")) {
+                return method.invoke(person, args);
+            }
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
